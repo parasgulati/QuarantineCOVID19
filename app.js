997269449f49a6881f7080053c8b0208c77e1649
Paras Gulati <44550739+parasgulati@users.noreply.github.com>
2020-04-07 15:18:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,100 @@
-const http=require('http');
-const api=require('backend/API.js');
-const port=process.env.PORT;
-api.set('port',port);
-const server=http.createServer(api);
-server.listen(port);
+const express=require('express');
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+const cors=require('cors');
+const API=express();
+const LoginDetails=require('./models/LoginDetails');
+const Updates=require('./models/Updates');
+
+mongoose.connect('mongodb+srv://COVID19:covidindia19@cluster-jqfui.mongodb.net/test?retryWrites=true&w=majority',{'useUnifiedTopology':true})
+.then(()=>{
+    console.log('database connected');
+})
+.catch(()=>{
+    console.log('database connectivity failed');
+});
+
+API.use(cors());
+API.use(bodyParser.json());
+API.use(bodyParser.urlencoded({extended: false}));
+
+API.post('/signup',(req,res,next)=>{
+    var post=req.body;
+    var details=new LoginDetails({
+        username:post.username,
+        password:post.password,
+        latitude:post.lattitude,
+        longitude:post.longitude,
+        imagePath:post.imagePath
+    });
+    LoginDetails.findOne({'username':post.username},function(err,data){
+        if(err)
+        {
+            console.log("error occured");
+        }
+        else
+        {
+            if(data==null)
+            {
+                details.save();
+                res.status(200).json({       
+                    message:'created'   
+                });
+                res.send();
+            }
+            else
+            {
+                res.status(200).json({
+                    message:'exists'
+                });
+                res.send();
+            }
+        }
+     });
+});
+
+API.post('/login',(req,res,next)=>{
+    var post=req.body;
+    LoginDetails.findOne({'username':post.username,'password':post.password},function(err,data){
+        if(err)
+        {
+            console.log("error occured");
+        }
+        else
+        {
+            if(data==null)
+            {
+                res.status(200).json({       
+                    message:'not'   
+                });
+                res.send();
+            }
+            else
+            {
+                res.status(200).json({
+                    message:'ok'
+                });
+                res.send(data);
+            }
+        }
+     });
+});
+
+API.post('/update',(req,res,next)=>{
+    var post=req.body;
+    var details=new Updates({
+        username:post.username,
+        date:new Date().toUTCString(),
+        status:post.status,
+        quarantine:post.quarantine
+    });
+    details.save();
+    res.status(200).json({
+        message:'added'
+    });
+    res.send();
+});
+    
+
+ var port = config.PORT || 3000;
+ API.listen(port)
