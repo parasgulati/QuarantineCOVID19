10f72a80b174afe3e76b4e1739434eef7887377c
Paras Gulati <44550739+parasgulati@users.noreply.github.com>
2020-04-07 13:50:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http=require('http');
-const api=require('./backend/API');
+const api=require('backend/API');
 const port=process.env.PORT||3000;
 api.set('port',port);
 const server=http.createServer(api);
