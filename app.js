1a46c365c6fe948bfc34d003056c8dbc95c9aff8
Paras Gulati <44550739+parasgulati@users.noreply.github.com>
2020-04-08 16:39:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ API.post('/signup',(req,res,next)=>{
     var details=new LoginDetails({
         username:post.username,
         password:post.password,
-        latitude:post.lattitude,
+        lat:post.lattitude,
         longitude:post.longitude,
         imagePath:post.imagePath
     });
