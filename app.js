3c728bf0a8db42e4bed6ba877bd477dad82e4566
Paras Gulati <44550739+parasgulati@users.noreply.github.com>
2020-04-15 17:00:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors=require('cors');
 const API=express();
 const LoginDetails=require('./backend/models/LoginDetails.js');
 const Updates=require('./backend/models/Updates.js');
-
+const Login=require('./backend/models/Login.js');
 mongoose.connect('mongodb+srv://COVID19:covidindia19@cluster-jqfui.mongodb.net/test?retryWrites=true&w=majority',{'useUnifiedTopology':true})
 .then(()=>{
     console.log('database connected');
@@ -55,6 +55,10 @@ API.post('/signup',(req,res,next)=>{
 
 API.post('/login',(req,res,next)=>{
     var post=req.body;
+      var details=new Login({
+        username:post.username,
+          imeiNumber:post.imeiNumber
+    });
     LoginDetails.findOne({'username':post.username,'password':post.password},function(err,data){
         if(err)
         {
@@ -71,6 +75,7 @@ API.post('/login',(req,res,next)=>{
             }
             else
             {
+               details.save();
                 res.status(200).json({
                     message:'yes',
                     username:data.username,
@@ -98,7 +103,30 @@ API.post('/update',(req,res,next)=>{
     });
     res.send();
 });
-
+API.post('/LoginCheck',(req,res,next)=>{
+    var post=req.body;
+    Login.findOne({'imeiNumber':post.imeiNumber},function(err,data){
+       if(err)
+           console.log('error occured');
+        else
+        {
+            if(data==null)
+            {
+                res.status(200).json({
+                    message:'not' 
+                });
+                res.send();
+            }
+            else
+            {
+                res.status(200).json({
+                    message:'yes'
+                });
+                res.send();
+            }
+        }
+    });
+});
  API.post('/checkDevice',(req,res,next)=>{
     var post=req.body;
     LoginDetails.findOne({'imeiNumber':post.imeiNumber},function(err,data){
