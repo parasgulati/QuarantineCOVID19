9cf1327a0b1fd5967b81eb475ce25de332ed64ba
Paras Gulati <44550739+parasgulati@users.noreply.github.com>
2020-04-29 23:21:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const API=express();
 const LoginDetails=require('./backend/models/LoginDetails.js');
 const Updates=require('./backend/models/Updates.js');
 const Login=require('./backend/models/Login.js');
+const Admin=require('./backend/models/Admin.js');
+const ControlLogin=require('./backend/models/ControlLogin.js');
+
 mongoose.connect('mongodb+srv://COVID19:covidindia19@cluster-jqfui.mongodb.net/test?retryWrites=true&w=majority',{'useUnifiedTopology':true})
 .then(()=>{
     console.log('database connected');
@@ -162,7 +165,7 @@ API.post('/LoginCheck',(req,res,next)=>{
 
 API.post('/searchPatients',(req,res,next)=>{
     var post=req.body;
-    var state,city,pincode,locality,district,quarantine,status;
+    var state,city,pincode,locality,district;
     if(post.state=='')
         state=0;
     else
@@ -187,33 +190,34 @@ API.post('/searchPatients',(req,res,next)=>{
         district=0;
     else
         district=1;
-    
-    if(post.quarantine=='')
-        quarantine=0;
-    else
-        quarantine=1;
-    
-    if(post.status=='')
-        status=0;
-    else
-        status=1;
+
     
     var json={};
-    
+    var j={};
+    j['$regex']=post.state;
+    j['$options']='i';
     if(state==1)
-        json['state']=post.state;
+        json['state']=j;
+    j={};
+    j['$regex']=post.city;
+    j['$options']='i';
     if(city==1)
-        json['city']=post.city;
+        json['city']=j;
+    j={};
+    j['$regex']=post.pincode;
+    j['$options']='i';
     if(pincode==1)
-        json['pincode']=post.pincode;
+        json['pincode']=j;
+    j={};
+    j['$regex']=post.locality;
+    j['$options']='i';
     if(locality==1)
-        json['locality']=post.locality;
+        json['locality']=j;
+    j={};
+    j['$regex']=post.district;
+    j['$options']='i';
     if(district==1)
-        json['district']=post.district;
-    if(quarantine==1)
-        json['quarantine']=post.quarantine;
-    if(status==1)
-        json['status']=post.status;
+        json['district']=j;
     
     LoginDetails.find(json,function(err,data){
         if(err)
@@ -231,6 +235,7 @@ API.post('/searchPatients',(req,res,next)=>{
             }
             else
             {
+                console.log(data);
                 res.send(data);
             }
         }
@@ -260,7 +265,212 @@ API.post('/PatientUpdates',(req,res,next)=>{
         }
 });
 });
+API.post('/searchPatients',(req,res,next)=>{
+    var post=req.body;
+    var state,city,pincode,locality,district;
+    if(post.state=='')
+        state=0;
+    else
+        state=1;
+    
+    if(post.city=='')
+        city=0;
+    else
+        city=1;
+    
+    if(post.pincode=='')
+        pincode=0;
+    else
+        pincode=1;
+    
+    if(post.locality=='')
+        locality=0;
+    else
+        locality=1;
+    
+    if(post.district=='')
+        district=0;
+    else
+        district=1;
 
+    
+    var json={};
+    var j={};
+    j['$regex']=post.state;
+    j['$options']='i';
+    if(state==1)
+        json['state']=j;
+    j={};
+    j['$regex']=post.city;
+    j['$options']='i';
+    if(city==1)
+        json['city']=j;
+    j={};
+    j['$regex']=post.pincode;
+    j['$options']='i';
+    if(pincode==1)
+        json['pincode']=j;
+    j={};
+    j['$regex']=post.locality;
+    j['$options']='i';
+    if(locality==1)
+        json['locality']=j;
+    j={};
+    j['$regex']=post.district;
+    j['$options']='i';
+    if(district==1)
+        json['district']=j;
+    
+    LoginDetails.find(json,function(err,data){
+        if(err)
+        {
+            console.log("error occured");
+        }
+        else
+        {
+            if(data==null)
+            {
+                res.status(200).json({       
+                    message:'Not Found'   
+                });
+                res.send();
+            }
+            else
+            {
+                res.send(data);
+            }
+        }
+     });
+});
 
+API.post('/NotQuarantinePatient',(req,res,next)=>{
+    var post=req.body;
+
+  Updates.find({$or:post.array},function(err,data){
+        if(err)
+        {
+            console.log("error occured");
+        }
+        else
+        {
+            if(data==null)
+            {
+                res.status(200).json({       
+                    message:'Not Found'   
+                });
+                res.send();
+            }
+            else
+            {
+                
+                res.send(data);
+            }
+        }
+});
+});
+
+API.post('/changeAdmin',(req,res,next)=>{
+    var post=req.body;
+    const Ad=new Admin({password:post.newAdminPassword});
+    Admin.find({password:post.oldAdminPassword},function(err,data){
+        if(err)
+            console.log('error occured while logining');
+        else
+        {
+            if(data.length==0)
+            {
+                res.status(200).json({
+                    message:'wrong'
+                })
+                res.send();
+            }
+            else
+            {
+                Admin.deleteMany({password:post.oldAdminPassword},function(er,dat)
+                {
+                    Ad.save();
+                    res.status(200).json({
+                        message:'changed'
+                    })
+                    res.send();    
+                });  
+            }
+        }
+    });    
+});
+
+API.post('/ControlSignup',(req,res,next)=>{
+    var post=req.body;
+
+    Admin.find({'password':post.adminPassword},function(err,d){
+        if(d.length!=0)
+        {
+                console.log('admin verified successfully');
+                const post1=new ControlLogin({
+                name:post.name,
+                email:post.email,
+                password:post.password
+                }); 
+            ControlLogin.findOne({'email':post.email},function(err,data){
+            if(err)
+            {
+                console.log("error occured");
+            }
+            else
+            {
+                if(data==null)
+                {
+                    post1.save();
+                    res.status(200).json({       
+                        message:'created'   
+                    });
+                    res.send();
+                }
+                else
+                {
+                    res.status(200).json({
+                        message:'exists'
+                    });
+                    res.send();
+                }
+            }
+         });
+        }
+        else
+        {
+            res.status(200).json({
+                message:'wrong'
+            });
+            res.send();
+        }
+    });
+});
+
+API.post('/ControlLogin',(req,res,next)=>{
+    var post=req.body;
+            ControlLogin.findOne({'email':post.email,'password':post.password},function(err,data){
+            if(err)
+            {
+                console.log("error occured");
+            }
+            else
+            {
+                if(data==null)
+                {
+                    res.status(200).json({       
+                        message:'wrong'   
+                    });
+                    res.send();
+                }
+                else
+                {
+                    res.status(200).json({
+                        message:'ok'
+                    });
+                    res.send();
+                }
+            }
+         });
+});
  var port =process.env.PORT;
- API.listen(port)
+ API.listen(port);
